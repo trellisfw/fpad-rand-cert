5bcef0ca4fea24439be6f58ab2abd9b6c8a43101
sanoel <noel.samuel.a@gmail.com>
2017-10-06 01:21:00 -0400

    bug fix

--- example.js
+++ example.js
@@ -8,8 +8,10 @@ var aud = randCert.generateAudit({
 		operations: templateAudit.scope.operations,
 		products_observed: templateAudit.scope.products_observed,
 	},
+	year: (parseInt(templateAudit.conditions_during_audit.operation_observed_date.slice(0,4))+1).toString()
 })
 
 console.log(aud.scope.operations)
 console.log(aud.scope.products_observed)
 console.log(aud.organization)
+console.log(aud.conditions_during_audit)
